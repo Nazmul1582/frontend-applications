70473e0642a932bcdcc459e941da3c28dcf79d30
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-02-22 23:59:20 +0600

    simple student attendance application

--- src/App.js
+++ src/App.js
@@ -1,89 +1,197 @@
+import { useState } from "react";
+
 function App() {
+  const [studentName, setStudentName] = useState("");
+  const [allStudents, setAllStudents] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editableStudent, setEditableStudent] = useState(null);
+
+  const addStudent = (e) => {
+    e.preventDefault();
+    if (!studentName) {
+      alert("Please type a valid name");
+      return;
+    }
+    setAllStudents([
+      {
+        id: Date.now() + "",
+        name: studentName,
+        isPresent: undefined,
+      },
+      ...allStudents,
+    ]);
+    setStudentName("");
+  };
+
+  const editHandler = (studentId) => {
+    setEditMode(true);
+    const toBeEdited = allStudents.find((student) => student.id === studentId);
+    setEditableStudent(toBeEdited);
+    setStudentName(toBeEdited.name);
+  };
+
+  const updateHandler = (e) => {
+    e.preventDefault();
+    if (!studentName) {
+      alert("Please enter a vali name");
+      return;
+    }
+    const newStudentsList = allStudents.map((student) => {
+      if (student.id === editableStudent.id) {
+        student.name = studentName;
+      }
+      return student;
+    });
+    setAllStudents(newStudentsList);
+    setEditMode(false);
+    setEditableStudent(null);
+    setStudentName("");
+  };
+
+  const removeStudent = (studentId) => {
+    const newStudentsList = allStudents.filter(
+      (student) => student.id !== studentId
+    );
+    setAllStudents(newStudentsList);
+  };
+
+  const makePresentHandler = (studentId) => {
+    const newStudentsList = allStudents.map((student) => {
+      if (student.id === studentId) {
+        if (student.isPresent === undefined) {
+          student.isPresent = true;
+        }
+      }
+      return student;
+    });
+    setAllStudents(newStudentsList);
+  };
+
+  const makeAbsentHandler = (studentId) => {
+    const newStudentsList = allStudents.map((student) => {
+      if (student.id === studentId) {
+        if (student.isPresent === undefined) {
+          student.isPresent = false;
+        }
+      }
+      return student;
+    });
+    setAllStudents(newStudentsList);
+  };
+
+  const accidentallyAddedHandler = (studentId) => {
+    const newStudentsList = allStudents.map((student) => {
+      if (student.id === studentId) {
+        student.isPresent = !student.isPresent;
+      }
+      return student;
+    });
+    setAllStudents(newStudentsList);
+  };
+
   return (
     <div className="min-h-screen  bg-gradient-to-r from-amber-500 to-amber-300">
       <h1 className="text-center text-4xl font-bold py-5">
         Simple Student Attendance Application
       </h1>
       <div className="flex flex-col items-center justify-center custom-height">
-        <form className="mb-10 bg-white shadow-xl p-5 rounded-xl text-center font-bold flex justify-center items-center gap-5">
+        <form
+          onSubmit={(e) => (editMode ? updateHandler(e) : addStudent(e))}
+          className="mb-10 bg-white shadow-xl p-5 rounded-xl text-center font-bold flex justify-center items-center gap-5"
+        >
           <input
             type="text"
+            value={studentName}
+            onChange={(e) => setStudentName(e.target.value)}
             className="p-2 rounded-md bg-slate-200 w-full border border-slate-200 focus:border-green-500 outline-0"
           />
-          <button className="btn bg-green-500 shadow-green-500/50">Add</button>
+          <button className="btn bg-green-500 shadow-green-500/50">
+            {editMode ? "Update" : "Add"}
+          </button>
         </form>
         <div className="grid grid-cols-[2fr_1fr_1fr] gap-5 p-5">
-          <div className="bg-white shadow-xl rounded-xl text-center p-3">
+          <div
+            className={`bg-white shadow-xl rounded-xl text-center p-3 ${
+              allStudents.length > 4 && "overflow-y-scroll"
+            }  max-h-[50vh]`}
+          >
             <h2 className="mb-5 text-xl font-bold">All Students</h2>
-            <div className="flex justify-between gap-3 font-semibold my-2 bg-slate-200 px-1 py-2 rounded-md">
-              <p>Student Name</p>
-              <div className=" flex gap-1">
-                <button className="btn  bg-indigo-500 shadow-indigo-500/50">
-                  Edit
-                </button>
-                <button className="btn  bg-cyan-500 shadow-cyan-500/50">
-                  Remove
-                </button>
-                <button className="btn  bg-amber-500 shadow-amber-500/50">
-                  Make present
-                </button>
-                <button className="btn  bg-pink-500 shadow-pink-500/50">
-                  Make Absent
-                </button>
+            {allStudents.map((student) => (
+              <div
+                key={student.id}
+                className="flex justify-between gap-3 font-semibold my-2 bg-slate-200 p-2 rounded-md"
+              >
+                <p>{student.name}</p>
+                <div className=" flex gap-1">
+                  <button
+                    onClick={() => editHandler(student.id)}
+                    className="btn  bg-indigo-500 shadow-indigo-500/50"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => removeStudent(student.id)}
+                    className="btn  bg-cyan-500 shadow-cyan-500/50"
+                  >
+                    Remove
+                  </button>
+                  <button
+                    onClick={() => makePresentHandler(student.id)}
+                    className="btn  bg-amber-500 shadow-amber-500/50"
+                  >
+                    Make present
+                  </button>
+                  <button
+                    onClick={() => makeAbsentHandler(student.id)}
+                    className="btn  bg-pink-500 shadow-pink-500/50"
+                  >
+                    Make Absent
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className="flex justify-between gap-3 font-semibold my-2 bg-slate-200 px-1 py-2 rounded-md">
-              <p>Student Name</p>
-              <div className=" flex gap-1">
-                <button className="btn  bg-indigo-500 shadow-indigo-500/50">
-                  Edit
-                </button>
-                <button className="btn  bg-cyan-500 shadow-cyan-500/50">
-                  Remove
-                </button>
-                <button className="btn  bg-amber-500 shadow-amber-500/50">
-                  Make present
-                </button>
-                <button className="btn  bg-pink-500 shadow-pink-500/50">
-                  Make Absent
-                </button>
-              </div>
-            </div>
-            <div className="flex justify-between gap-3 font-semibold my-2 bg-slate-200 px-1 py-2 rounded-md">
-              <p>Student Name</p>
-              <div className=" flex gap-1">
-                <button className="btn  bg-indigo-500 shadow-indigo-500/50">
-                  Edit
-                </button>
-                <button className="btn  bg-cyan-500 shadow-cyan-500/50">
-                  Remove
-                </button>
-                <button className="btn  bg-amber-500 shadow-amber-500/50">
-                  Make present
-                </button>
-                <button className="btn  bg-pink-500 shadow-pink-500/50">
-                  Make Absent
-                </button>
-              </div>
-            </div>
+            ))}
           </div>
-          <div className="bg-white shadow-xl rounded-xl text-center p-3">
+          <div
+            className={`bg-white shadow-xl rounded-xl text-center p-3 ${
+              allStudents.length > 4 && "overflow-y-scroll"
+            }  max-h-[50vh]`}
+          >
             <h2 className="mb-5 text-xl font-bold">Present Students</h2>
-            <div className="flex flex-wrap gap-1 font-semibold my-2 bg-slate-200 p-2 rounded-md">
-              <p>Student Name</p>
-              <button className="btn  bg-indigo-500 shadow-indigo-500/50">
-                Accidentally Added
-              </button>
-            </div>
+            {allStudents.map(
+              (student) =>
+                student.isPresent && (
+                  <div className="flex justify-between flex-wrap gap-1 font-semibold my-2 bg-slate-200 p-2 rounded-md">
+                    <p>{student.name}</p>
+                    <button
+                      onClick={() => accidentallyAddedHandler(student.id)}
+                      className="btn  bg-green-500 shadow-green-500/50"
+                    >
+                      Accidentally Added
+                    </button>
+                  </div>
+                )
+            )}
           </div>
-          <div className="bg-white shadow-xl rounded-xl text-center p-3">
+          <div
+            className={`bg-white shadow-xl rounded-xl text-center p-3 ${
+              allStudents.length > 4 && "overflow-y-scroll"
+            }  max-h-[50vh]`}
+          >
             <h2 className="mb-5 text-xl font-bold">Absent Students</h2>
-            <div className="flex flex-wrap gap-1 font-semibold my-2 bg-slate-200 p-2 rounded-md">
-              <p>Student Name</p>
-              <button className="btn  bg-indigo-500 shadow-indigo-500/50">
-                Accidentally Added
-              </button>
-            </div>
+            {allStudents.map(
+              (student) =>
+                student.isPresent === false && (
+                  <div className="flex justify-between flex-wrap gap-1 font-semibold my-2 bg-slate-200 p-2 rounded-md">
+                    <p>{student.name}</p>
+                    <button
+                      onClick={() => accidentallyAddedHandler(student.id)}
+                      className="btn  bg-rose-500 shadow-rose-500/50"
+                    >
+                      Accidentally Added
+                    </button>
+                  </div>
+                )
+            )}
           </div>
         </div>
       </div>
@@ -92,60 +200,3 @@ function App() {
 }
 
 export default App;
-
-// <form
-//   onSubmit={(e) => {
-//     editMode ? updateHandler(e) : createHandler(e);
-//   }}
-//   className="mb-10 bg-white shadow-xl p-5 rounded-xl text-center font-bold flex justify-center items-center gap-5"
-// >
-//   <input
-//     type="text"
-//     value={noteTitle}
-//     onChange={(e) => setNoteTitle(e.target.value)}
-//     className="p-2 rounded-md bg-slate-200 w-full border border-slate-200 focus:border-cyan-500 outline-0"
-//   />
-//   <button
-//     type="submit"
-//     className="btn bg-cyan-500 shadow-cyan-500/50"
-//   >
-//     {editMode ? "Update" : "Add"}
-//   </button>
-// </form>
-
-// <div
-//   className={`bg-white shadow-xl rounded-xl text-center font-bold p-5  ${
-//     notes.length === 0 ? "hidden" : "block"
-//   }`}
-// >
-//   <div
-//     className={` max-h-[300px] ${
-//       notes.length >= 4 && "overflow-y-scroll"
-//     }`}
-//   >
-//     {notes.map((note) => (
-//       <div
-//         key={note.id}
-//         className="bg-slate-200 flex gap-5 items-center justify-between rounded-md p-3 my-3"
-//       >
-//         <p className="text-xl">{note.title}</p>
-//         <div className="flex gap-3">
-//           <button
-//             onClick={() => editHandler(note.id)}
-//             type="button"
-//             className="btn  bg-indigo-500 shadow-indigo-500/50"
-//           >
-//             Edit
-//           </button>
-//           <button
-//             onClick={() => removeHandler(note.id)}
-//             type="button"
-//             className="btn  bg-pink-500 shadow-pink-500/50"
-//           >
-//             Remove
-//           </button>
-//         </div>
-//       </div>
-//     ))}
-//   </div>
-// </div>
